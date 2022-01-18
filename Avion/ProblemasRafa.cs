a2b5fc3ead7dcc9a4708d5f941b8967e190f0600
191G0239@rcarbonifera.tecnm.mx <191G0239@rcarbonifera.tecnm.mx>
2022-01-18 15:53:55 -0600

    Agregar archivos de proyecto.

---
+++ Avion/ProblemasRafa.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avion
+{
+    class ProblemasRafa
+    {
+        
+
+        public string QuitarAcentos(string PalabraAcentuada)
+        {
+            string PalabraNoAcentuada = "";
+            for (int i = 0; i < PalabraAcentuada.Length; i++)
+            {
+                if (PalabraAcentuada[i] == 'á')
+                {
+                    PalabraNoAcentuada += 'a';
+                }
+                else if (PalabraAcentuada[i] == 'é')
+                {
+                    PalabraNoAcentuada += 'e';
+                }
+                else if (PalabraAcentuada[i] == 'í')
+                {
+                    PalabraNoAcentuada += 'i';
+                }
+                else if (PalabraAcentuada[i] == 'ó')
+                {
+                    PalabraNoAcentuada += 'o';
+                }
+                else if (PalabraAcentuada[i] == 'ú')
+                {
+                    PalabraNoAcentuada += 'u';
+                }
+                else
+                {
+                    PalabraNoAcentuada += PalabraAcentuada[i];
+                }
+            }
+
+            return PalabraNoAcentuada;
+        }
+
+
+        public byte[] LlenarArreglo(byte[] Arreglo)
+        {
+            byte[] ArregloLleno = new byte[100];
+            for (int i = 0; i == 100; i++)
+            {
+                ArregloLleno[i] = (byte)i;
+            }
+            return ArregloLleno;
+        }
+
+
+        public byte[] NumAleatorios()
+        {
+            Random r = new Random();
+            byte[] ArregloAlAzar = new byte[20];
+            for (int i = 0; i == 19; i++)
+            {
+                ArregloAlAzar[i] += (byte)r.Next(0, 101);
+            }
+            return ArregloAlAzar;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ PriPanPrd/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PriPanPrd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPan_KeyDown(object sender, KeyEventArgs e)
+        {
+            double escala = double.Parse(txtPan.Text);
+            if (e.Key == Key.Enter)
+            {
+                if (escala >= 0 && escala <= 100)
+                {
+                    RecPan.ScaleY = escala;
+                }
+                else
+                    MessageBox.Show("Atencion", "Solo numeros dentro del 0 al 100", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void txtPri_KeyDown(object sender, KeyEventArgs e)
+        {
+            double escala = double.Parse(txtPri.Text);
+            if (e.Key == Key.Enter)
+            {
+                if (escala >= 0 && escala <= 100)
+                {
+                    RecPri.ScaleY = escala;
+                }
+                else
+                    MessageBox.Show("Atencion", "Solo numeros dentro del 0 al 100", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void txtPrd_KeyDown(object sender, KeyEventArgs e)
+        {
+            double escala = double.Parse(txtPrd.Text);
+            if (e.Key == Key.Enter)
+            {
+                if (escala >= 0 && escala <= 100)
+                {
+                    RecPrd.ScaleY = escala;
+                }
+                else
+                    MessageBox.Show("Atencion", "Solo numeros dentro del 0 al 100", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        //private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        //{
+
+        //}
+
+
+
+    }
+}
